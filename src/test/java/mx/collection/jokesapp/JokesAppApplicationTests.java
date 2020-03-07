6dff9fc752c3bc375aec86bbd4577f2a8bd5be22
candidate.gigrt <miao_mx@yahoo.com>
2020-03-07 18:40:08 +0800

    Initial Structure with Web and Thymeleaf dependency.

---
+++ src/test/java/mx/collection/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.collection.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
